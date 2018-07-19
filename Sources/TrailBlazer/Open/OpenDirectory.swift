a596e9ee712d0b26f8b6e139acaf705854ca3d79
Jacob Williams <ponyboy47@gmail.com>
2018-07-19 15:22:01 -0600

    Set up recursive permissions functions

--- Sources/TrailBlazer/Open/OpenDirectory.swift
+++ Sources/TrailBlazer/Open/OpenDirectory.swift
@@ -44,4 +44,95 @@ public extension Open where PathType: DirectoryPath {
 
         try changeRecursive(owner: uid, group: gid)
     }
+
+    public func changeRecursive(permissions: FileMode) throws {
+        try change(permissions: permissions)
+
+        for path in self {
+            if let dir = DirectoryPath(path) {
+                guard !["..", "."].contains(dir.lastComponent) else { continue }
+                try dir.changeRecursive(permissions: permissions)
+            } else {
+                try path.change(permissions: permissions)
+            }
+        }
+    }
+
+    public func changeRecursive(owner: FilePermissions, group: FilePermissions, others: FilePermissions, uid: Bool, gid: Bool, sticky: Bool) throws {
+        try changeRecursive(permissions: FileMode(owner: owner, group: group, others: others, uid: uid, gid: gid, sticky: sticky))
+    }
+
+    public func changeRecursive(owner perms: FilePermissions...) throws {
+        let current = permissions
+        try changeRecursive(owner: FilePermissions(rawValue: perms.reduce(0, { $0 | $1.rawValue })), group: current.group, others: current.others, uid: current.uid, gid: current.gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(group perms: FilePermissions...) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: FilePermissions(rawValue: perms.reduce(0, { $0 | $1.rawValue })), others: current.others, uid: current.uid, gid: current.gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(others perms: FilePermissions...) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: current.group, others: FilePermissions(rawValue: perms.reduce(0, { $0 | $1.rawValue })), uid: current.uid, gid: current.gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(ownerGroup perms: FilePermissions...) throws {
+        let perm = FilePermissions(rawValue: perms.reduce(0, { $0 | $1.rawValue }))
+        let current = permissions
+        try changeRecursive(owner: perm, group: perm, others: current.others, uid: current.uid, gid: current.gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(ownerOthers perms: FilePermissions...) throws {
+        let perm = FilePermissions(rawValue: perms.reduce(0, { $0 | $1.rawValue }))
+        let current = permissions
+        try changeRecursive(owner: perm, group: current.group, others: perm, uid: current.uid, gid: current.gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(groupOthers perms: FilePermissions...) throws {
+        let perm = FilePermissions(rawValue: perms.reduce(0, { $0 | $1.rawValue }))
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: perm, others: perm, uid: current.uid, gid: current.gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(ownerGroupOthers perms: FilePermissions...) throws {
+        let perm = FilePermissions(rawValue: perms.reduce(0, { $0 | $1.rawValue }))
+        let current = permissions
+        try changeRecursive(owner: perm, group: perm, others: perm, uid: current.uid, gid: current.gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(uid: Bool) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: current.group, others: current.others, uid: uid, gid: current.gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(gid: Bool) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: current.group, others: current.others, uid: current.uid, gid: gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(sticky: Bool) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: current.group, others: current.others, uid: current.uid, gid: current.gid, sticky: sticky)
+    }
+
+    public func changeRecursive(uid: Bool, gid: Bool) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: current.group, others: current.others, uid: uid, gid: gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(uid: Bool, sticky: Bool) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: current.group, others: current.others, uid: uid, gid: current.gid, sticky: sticky)
+    }
+
+    public func changeRecursive(gid: Bool, sticky: Bool) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: current.group, others: current.others, uid: current.uid, gid: gid, sticky: sticky)
+    }
+
+    public func changeRecursive(uid: Bool, gid: Bool, sticky: Bool) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: current.group, others: current.others, uid: uid, gid: gid, sticky: sticky)
+    }
 }
--- Sources/TrailBlazer/Path/Path+Directory.swift
+++ Sources/TrailBlazer/Path/Path+Directory.swift
@@ -280,6 +280,108 @@ public class DirectoryPath: _Path, Openable, Sequence, IteratorProtocol {
         try changeRecursive(owner: uid, group: gid)
     }
 
+    public func changeRecursive(permissions: FileMode) throws {
+        guard exists else { return }
+
+        try change(permissions: permissions)
+
+        let unopened = dir == nil
+        if unopened {
+            try open()
+        }
+
+        for path in self {
+            if let dir = DirectoryPath(path) {
+                guard !["..", "."].contains(dir.lastComponent) else { continue }
+                try dir.changeRecursive(permissions: permissions)
+            } else {
+                try path.change(permissions: permissions)
+            }
+        }
+
+        if unopened {
+            try close()
+        }
+    }
+
+    public func changeRecursive(owner: FilePermissions, group: FilePermissions, others: FilePermissions, uid: Bool, gid: Bool, sticky: Bool) throws {
+        try changeRecursive(permissions: FileMode(owner: owner, group: group, others: others, uid: uid, gid: gid, sticky: sticky))
+    }
+
+    public func changeRecursive(owner perms: FilePermissions...) throws {
+        let current = permissions
+        try changeRecursive(owner: FilePermissions(rawValue: perms.reduce(0, { $0 | $1.rawValue })), group: current.group, others: current.others, uid: current.uid, gid: current.gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(group perms: FilePermissions...) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: FilePermissions(rawValue: perms.reduce(0, { $0 | $1.rawValue })), others: current.others, uid: current.uid, gid: current.gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(others perms: FilePermissions...) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: current.group, others: FilePermissions(rawValue: perms.reduce(0, { $0 | $1.rawValue })), uid: current.uid, gid: current.gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(ownerGroup perms: FilePermissions...) throws {
+        let perm = FilePermissions(rawValue: perms.reduce(0, { $0 | $1.rawValue }))
+        let current = permissions
+        try changeRecursive(owner: perm, group: perm, others: current.others, uid: current.uid, gid: current.gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(ownerOthers perms: FilePermissions...) throws {
+        let perm = FilePermissions(rawValue: perms.reduce(0, { $0 | $1.rawValue }))
+        let current = permissions
+        try changeRecursive(owner: perm, group: current.group, others: perm, uid: current.uid, gid: current.gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(groupOthers perms: FilePermissions...) throws {
+        let perm = FilePermissions(rawValue: perms.reduce(0, { $0 | $1.rawValue }))
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: perm, others: perm, uid: current.uid, gid: current.gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(ownerGroupOthers perms: FilePermissions...) throws {
+        let perm = FilePermissions(rawValue: perms.reduce(0, { $0 | $1.rawValue }))
+        let current = permissions
+        try changeRecursive(owner: perm, group: perm, others: perm, uid: current.uid, gid: current.gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(uid: Bool) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: current.group, others: current.others, uid: uid, gid: current.gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(gid: Bool) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: current.group, others: current.others, uid: current.uid, gid: gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(sticky: Bool) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: current.group, others: current.others, uid: current.uid, gid: current.gid, sticky: sticky)
+    }
+
+    public func changeRecursive(uid: Bool, gid: Bool) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: current.group, others: current.others, uid: uid, gid: gid, sticky: current.sticky)
+    }
+
+    public func changeRecursive(uid: Bool, sticky: Bool) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: current.group, others: current.others, uid: uid, gid: current.gid, sticky: sticky)
+    }
+
+    public func changeRecursive(gid: Bool, sticky: Bool) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: current.group, others: current.others, uid: current.uid, gid: gid, sticky: sticky)
+    }
+
+    public func changeRecursive(uid: Bool, gid: Bool, sticky: Bool) throws {
+        let current = permissions
+        try changeRecursive(owner: current.owner, group: current.group, others: current.others, uid: uid, gid: gid, sticky: sticky)
+    }
+
     public static func + (lhs: DirectoryPath, rhs: String) -> GenericPath {
         return lhs + GenericPath(rhs)
     }
