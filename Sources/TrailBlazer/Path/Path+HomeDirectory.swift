e71dc3baec7c2dcb93fab1a5cf3826947f163942
Jacob Williams <ponyboy47@gmail.com>
2018-07-24 10:18:13 -0600

    Correct the implementation of get(pw|gr)(uid|gid|nam) C API calls to set errno to 0 beforehand

--- Sources/TrailBlazer/Path/Path+HomeDirectory.swift
+++ Sources/TrailBlazer/Path/Path+HomeDirectory.swift
@@ -83,17 +83,29 @@ extension Path {
     }
     /// Returns a passwd structure for the specified username
     static func getUserInfo(_ username: String) -> passwd? {
+        // getpwnam(2) documentation says "If one wants to check errno after
+        // the call, it should be set to zero before the call."
+        errno = 0
         return getpwnam(username)?.pointee
     }
     static func getUserInfo(_ uid: uid_t) -> passwd? {
+        // getpwuid(2) documentation says "If one wants to check errno after
+        // the call, it should be set to zero before the call."
+        errno = 0
         return getpwuid(uid)?.pointee
     }
 
     // Returns a group structure for the specified groupname
     static func getGroupInfo(_ groupname: String) -> group? {
+        // getgrnam(2) documentation says "If one wants to check errno after
+        // the call, it should be set to zero before the call."
+        errno = 0
         return getgrnam(groupname)?.pointee
     }
     static func getGroupInfo(_ gid: gid_t) -> group? {
+        // getgrgid(2) documentation says "If one wants to check errno after
+        // the call, it should be set to zero before the call."
+        errno = 0
         return getgrgid(gid)?.pointee
     }
 }
