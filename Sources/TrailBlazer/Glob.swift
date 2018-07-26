9e2df7ac6026347182ed19eda22b32be3e92efcc
Jacob Williams <ponyboy47@gmail.com>
2018-07-26 10:26:55 -0600

    Reorganized and commented code

--- Sources/TrailBlazer/Glob.swift
+++ Sources/TrailBlazer/Glob.swift
@@ -1,51 +1,113 @@
 import Cglob
 let cGlob = Cglob.glob
-let cGlobFree = Cglob.globfree
+public let cGlobFree = Cglob.globfree
 
+// A reusable Glob struct
 private var globalGlob = Glob()
 
+/** Locates all paths matching the pattern specified
+
+    - Parameter pattern: The path string and glob pattern to use for finding paths
+    - Parameter flags: Glob flags to customize the globbing behavior (see GlobFlags)
+    - Parameter errorClosure: A closure used if a path in the glob causes an error. Returning a nonZero integer will cause the glob function to throw an error
+    - Throws: GlobError or OpenDirectoryError
+    - Returns: A Glob object that contains the paths matching the pattern as well as other information about the glob
+*/
 public func glob(pattern: String, flags: GlobFlags = [], errorClosure: GlobError.ErrorHandler? = nil) throws -> Glob {
     try glob(pattern: pattern, flags: flags, errorClosure: errorClosure, glob: &globalGlob)
     return globalGlob
 }
 
+/** Locates all paths matching the pattern specified
+
+    - Parameter pattern: The path string and glob pattern to use for finding paths
+    - Parameter flags: Glob flags to customize the globbing behavior (see GlobFlags)
+    - Parameter errorClosure: A closure used if a path in the glob causes an error. Returning a nonZero integer will cause the glob function to throw an error
+    - Parameter glob: The glob object into which the glob results are stored
+    - Throws: GlobError or OpenDirectoryError
+*/
 public func glob(pattern: String, flags: GlobFlags = [], errorClosure: GlobError.ErrorHandler? = nil, glob: inout Glob) throws {
-    let globResult = cGlob(pattern, flags.rawValue, errorClosure, &glob._glob)
-    guard globResult == 0 else { throw GlobError.getError(globResult) }
-}
-
-extension Path {
-    public static func glob(pattern: String, flags: GlobFlags = [], errorClosure: GlobError.ErrorHandler? = nil) throws -> Glob {
-        return try TrailBlazer.glob(pattern: pattern, flags: flags, errorClosure: errorClosure)
-    }
-
-    public static func glob(pattern: String, flags: GlobFlags = [], errorClosure: GlobError.ErrorHandler? = nil, glob: inout Glob) throws {
-        try TrailBlazer.glob(pattern: pattern, flags: flags, errorClosure: errorClosure, glob: &glob)
+    let globResult = cGlob(pattern, flags.rawValue, errorClosure, glob._glob)
+    guard globResult == 0 else {
+        do {
+            throw GlobError.getError(globResult)
+        // There may be an error thrown from the underlying opendir/malloc
+        // calls, so if it's an unknown GlobError try throwing the
+        // OpenDirectoryError (malloc errors hopefully won't ever happen)
+        } catch GlobError.unknown {
+            throw OpenDirectoryError.getError()
+        }
     }
 }
 
 extension DirectoryPath {
+    /** Locates all paths matching the pattern specified using this DirectoryPath as the base directory for the glob
+
+        - Parameter pattern: The glob pattern to use for finding paths
+        - Parameter flags: Glob flags to customize the globbing behavior (see GlobFlags)
+        - Parameter errorClosure: A closure used if a path in the glob causes an error. Returning a nonZero integer will cause the glob function to throw an error
+        - Throws: GlobError or OpenDirectoryError
+        - Returns: A Glob object that contains the paths matching the pattern as well as other information about the glob
+    */
     public func glob(pattern: String, flags: GlobFlags = [], errorClosure: GlobError.ErrorHandler? = nil) throws -> Glob {
         return try TrailBlazer.glob(pattern: (self + pattern).string, flags: flags, errorClosure: errorClosure)
     }
 
+    /** Locates all paths matching the pattern specified using this DirectoryPath as the base directory for the glob
+
+        - Parameter pattern: The glob pattern to use for finding paths
+        - Parameter flags: Glob flags to customize the globbing behavior (see GlobFlags)
+        - Parameter errorClosure: A closure used if a path in the glob causes an error. Returning a nonZero integer will cause the glob function to throw an error
+        - Parameter glob: The glob object into which the glob results are stored
+        - Throws: GlobError or OpenDirectoryError
+    */
     public func glob(pattern: String, flags: GlobFlags = [], errorClosure: GlobError.ErrorHandler? = nil, glob: inout Glob) throws {
         try TrailBlazer.glob(pattern: (self + pattern).string, flags: flags, errorClosure: errorClosure, glob: &glob)
     }
 }
 
+/// The construct used and returned by globbing
 public class Glob {
-    fileprivate var _glob: glob_t = glob_t()
+    /// A pointer to the underlying glob_t struct used for the C glob(3) API calls
+    fileprivate var _glob: UnsafeMutablePointer<glob_t>
+    /// Whether or not this library owns the underlying glob_t struct and can
+    /// safely free it's memory on deinitialization
+    private let owned: Bool
 
-    var count: UInt { return _glob.gl_pathc }
-    var matches: PathCollection {
+    /// The number of matches that should be found in the matches collection
+    public var trueCount: Int { return Int(count - offset) }
+    /// The number of matches + the offset
+    public var count: UInt { return _glob.pointee.gl_pathc }
+    /** The paths that matched the globbing pattern
+
+        NOTE: Since this is a computed variable, it would be more efficient to
+        store this into a variable for use in your program if you intend to use
+        it more than once. (Getting the files and then getting the directories
+        constitutes multiple accesses and therefore multiple computations)
+    */
+    public var matches: PathCollection {
+        // Create the children collection
         let children = PathCollection()
 
-        var item = _glob.gl_pathv
-        for _ in 0...count {
+        // Array of Strings of the matched paths. (char **)
+        var item = _glob.pointee.gl_pathv
+
+        // Skip the offset number of matches since those are reserved and will be nil
+        for _ in 0..<offset {
+            item = item?.successor()
+        }
+
+        // Go through the remaining items, get the path type, and append it to
+        // the relevant children array
+        for _ in offset...count {
+            // Make sure the item pointed to is not nil or else we've hit the
+            // end. The glob(3) docs say the array is null-terminated.
             guard let pointee = item?.pointee else { break }
+
+            // Cast the char * pointee to a swift String
             let path = String(cString: pointee)
 
+            // Get the path type and append it to the corresponding array
             if let file = FilePath(path) {
                 children.files.append(file)
             } else if let dir = DirectoryPath(path) {
@@ -54,46 +116,83 @@ public class Glob {
                 children.other.append(GenericPath(path))
             }
 
+            // Advance to the next item in the array
             item = item?.successor()
         }
 
         return children
     }
-    var offset: UInt { return _glob.gl_offs }
-    var closedir: (@convention(c) (UnsafeMutableRawPointer?) -> ()) {
-        get { return _glob.gl_closedir }
+    /** The number of reserved items at the beginning of the matches in the
+        underlying glob_t struct. Reserved items are ignored/skipped in the
+        matches variable of this Glob object.
+    */
+    public var offset: UInt { return _glob.pointee.gl_offs }
+    /// The C function to use to close directories (default is closedir(2))
+    public var closedir: (@convention(c) (UnsafeMutableRawPointer?) -> ()) {
+        get { return _glob.pointee.gl_closedir }
         set {
-            _glob.gl_closedir = newValue
+            _glob.pointee.gl_closedir = newValue
         }
     }
-    var readdir: (@convention(c) (UnsafeMutableRawPointer?) -> UnsafeMutableRawPointer?) {
-        get { return _glob.gl_readdir }
+    /// The C function used to read directories (default is readdir(2))
+    public var readdir: (@convention(c) (UnsafeMutableRawPointer?) -> UnsafeMutableRawPointer?) {
+        get { return _glob.pointee.gl_readdir }
         set {
-            _glob.gl_readdir = newValue
+            _glob.pointee.gl_readdir = newValue
         }
     }
-    var opendir: (@convention(c) (UnsafePointer<CChar>?) -> UnsafeMutableRawPointer?) {
-        get { return _glob.gl_opendir }
+    /// The C function used to open directories (default is opendir(2))
+    public var opendir: (@convention(c) (UnsafePointer<CChar>?) -> UnsafeMutableRawPointer?) {
+        get { return _glob.pointee.gl_opendir }
         set {
-            _glob.gl_opendir = newValue
+            _glob.pointee.gl_opendir = newValue
         }
     }
-    var lstat: (@convention(c) (UnsafePointer<CChar>?, UnsafeMutableRawPointer?) -> FileDescriptor) {
-        get { return _glob.gl_lstat }
+    /// The C function used to lstat directories (default is lstat(2))
+    public var lstat: (@convention(c) (UnsafePointer<CChar>?, UnsafeMutableRawPointer?) -> FileDescriptor) {
+        get { return _glob.pointee.gl_lstat }
         set {
-            _glob.gl_lstat = newValue
+            _glob.pointee.gl_lstat = newValue
         }
     }
-    var stat: (@convention(c) (UnsafePointer<CChar>?, UnsafeMutableRawPointer?) -> FileDescriptor) {
-        get { return _glob.gl_stat }
+    /// The C function used to stat directories (default is stat(2))
+    public var stat: (@convention(c) (UnsafePointer<CChar>?, UnsafeMutableRawPointer?) -> FileDescriptor) {
+        get { return _glob.pointee.gl_stat }
         set {
-            _glob.gl_stat = newValue
+            _glob.pointee.gl_stat = newValue
         }
+    }
+    /// The flags used by the glob (if it was previously used before)
+    public var flags: GlobFlags { return GlobFlags(rawValue: _glob.pointee.gl_flags) }
+
+    /// Initializes with an empty glob_t struct
+    public init() {
+        _glob = UnsafeMutablePointer.allocate(capacity: 1)
+        _glob.initialize(to: glob_t())
+
+        // Since we have sole control over the pointer, it's safe to deallocate
+        // it at deinitialization
+        owned = true
     }
 
-    public init() {}
+    /** Initializes with a pointer to the specified glob_t struct. Only use
+        this initializer if you have a specific reason to own the management of
+        your glob_t struct. You MUST call globfree(3) (or cGlobFree) with your
+        glob_t struct when you are done or it will be a memory leak
+    */
+    public init(glob: UnsafeMutablePointer<glob_t>) {
+        _glob = glob
 
+        // If the user is passing their own glob_t pointer, then we do not own
+        // the management of it and they will have to free it themselves
+        owned = false
+    }
+
+    // When this object is deconstructed, be sure to free the glob pointer (or
+    // you'll have a memory leak)
     deinit {
-        cGlobFree(&_glob)
+        if owned {
+            cGlobFree(_glob)
+        }
     }
 }
--- Sources/TrailBlazer/PathCollection.swift
+++ Sources/TrailBlazer/PathCollection.swift
@@ -1,9 +1,18 @@
+/** An object containing a collection of files, directories, and other paths
+    from some kind of traversal or enumeration (ie: globbing or getting
+    directory children)
+*/
 public class PathCollection: Equatable, CustomStringConvertible {
+    /// The file paths
     public internal(set) var files: [FilePath]
+    /// The directory paths
     public internal(set) var directories: [DirectoryPath]
+    /// Other paths
     public internal(set) var other: [GenericPath]
 
+    /// Whether or not this collection is empty
     public var isEmpty: Bool { return files.isEmpty && directories.isEmpty && other.isEmpty }
+    /// The number of paths stored in this collection
     public var count: Int { return files.count + directories.count + other.count }
 
     public var description: String {
@@ -20,18 +29,25 @@ public class PathCollection: Equatable, CustomStringConvertible {
         return str.joined(separator: "\n\n")
     }
 
+    /** Initializer
+        - Parameter files: The FilePaths to begin with as a part of this collection
+        - Parameter directories: The DirectoryPaths to begin with as a part of this collection
+        - Parameter other: The remaining Paths to begin with as a part of this collection
+    */
     init(files: [FilePath] = [], directories: [DirectoryPath] = [], other: [GenericPath] = []) {
         self.files = files
         self.directories = directories
         self.other = other
     }
 
+    /// Combines the items from one PathCollection into this PathCollection
     public static func += (lhs: inout PathCollection, rhs: PathCollection) {
         lhs.files += rhs.files
         lhs.directories += rhs.directories
         lhs.other += rhs.other
     }
 
+    /// Whether or not two PathCollections are equivalent
     public static func == (lhs: PathCollection, rhs: PathCollection) -> Bool {
         return lhs.files == rhs.files && lhs.directories == rhs.directories && lhs.other == rhs.other
     }
