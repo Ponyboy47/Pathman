d6d6240d0fea32750c38dc0cc27cf2daff06d881
Jacob Williams <ponyboy47@gmail.com>
2018-07-26 10:25:21 -0600

    Moved CustomStringConvertible conformance to an extension

--- Sources/TrailBlazer/COptions/OpenFileFlags.swift
+++ Sources/TrailBlazer/COptions/OpenFileFlags.swift
@@ -4,8 +4,36 @@ import Glibc
 import Darwin
 #endif
 
-public struct OpenFileFlags: OptionSet, CustomStringConvertible {
+public struct OpenFileFlags: OptionSet {
     public let rawValue: OptionInt
+
+    public static let append = OpenFileFlags(rawValue: O_APPEND)
+    public static let async = OpenFileFlags(rawValue: O_ASYNC)
+    public static let closeOnExec = OpenFileFlags(rawValue: O_CLOEXEC)
+    public static let create = OpenFileFlags(rawValue: O_CREAT)
+    public static let directory = OpenFileFlags(rawValue: O_DIRECTORY)
+    public static let excl = OpenFileFlags(rawValue: O_EXCL)
+    public static let noCTTY = OpenFileFlags(rawValue: O_NOCTTY)
+    public static let noFollow = OpenFileFlags(rawValue: O_NOFOLLOW)
+    public static let nonBlock = OpenFileFlags(rawValue: O_NONBLOCK)
+    public static let nDelay = OpenFileFlags(rawValue: O_NDELAY)
+    public static let truncate = OpenFileFlags(rawValue: O_TRUNC)
+    #if os(Linux)
+    public static let dsync = OpenFileFlags(rawValue: O_DSYNC)
+    public static let sync = OpenFileFlags(rawValue: O_SYNC)
+    #else
+    public static let sharedLock = OpenFileFlags(rawValue: O_SHLOCK)
+    public static let exclusiveLock = OpenFileFlags(rawValue: O_EXLOCK)
+    public static let symlink = OpenFileFlags(rawValue: O_SYMLINK)
+    public static let evtOnly = OpenFileFlags(rawValue: O_EVTONLY)
+    #endif
+
+    public init(rawValue: OptionInt) {
+        self.rawValue = rawValue
+    }
+}
+
+extension OpenFileFlags: CustomStringConvertible {
     public var description: String {
         var flags: [String] = []
 
@@ -64,31 +92,10 @@ public struct OpenFileFlags: OptionSet, CustomStringConvertible {
         }
         #endif
 
-        return "\(type(of: self))(\(flags.joined(separator: ", ")))"
-    }
+        if flags.isEmpty {
+            flags.append("none")
+        }
 
-    public static let append = OpenFileFlags(rawValue: O_APPEND)
-    public static let async = OpenFileFlags(rawValue: O_ASYNC)
-    public static let closeOnExec = OpenFileFlags(rawValue: O_CLOEXEC)
-    public static let create = OpenFileFlags(rawValue: O_CREAT)
-    public static let directory = OpenFileFlags(rawValue: O_DIRECTORY)
-    public static let excl = OpenFileFlags(rawValue: O_EXCL)
-    public static let noCTTY = OpenFileFlags(rawValue: O_NOCTTY)
-    public static let noFollow = OpenFileFlags(rawValue: O_NOFOLLOW)
-    public static let nonBlock = OpenFileFlags(rawValue: O_NONBLOCK)
-    public static let nDelay = OpenFileFlags(rawValue: O_NDELAY)
-    public static let truncate = OpenFileFlags(rawValue: O_TRUNC)
-    #if os(Linux)
-    public static let dsync = OpenFileFlags(rawValue: O_DSYNC)
-    public static let sync = OpenFileFlags(rawValue: O_SYNC)
-    #else
-    public static let sharedLock = OpenFileFlags(rawValue: O_SHLOCK)
-    public static let exclusiveLock = OpenFileFlags(rawValue: O_EXLOCK)
-    public static let symlink = OpenFileFlags(rawValue: O_SYMLINK)
-    public static let evtOnly = OpenFileFlags(rawValue: O_EVTONLY)
-    #endif
-
-    public init(rawValue: OptionInt) {
-        self.rawValue = rawValue
+        return "\(type(of: self))(\(flags.joined(separator: ", ")), rawValue: \(rawValue))"
     }
 }
