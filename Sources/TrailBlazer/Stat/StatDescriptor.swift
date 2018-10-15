664e3b065a17f42ce2f347d0e943c9e9478c966c
Jacob Williams <ponyboy47@gmail.com>
2018-10-14 22:53:40 -0600

    Remove unused code

--- Sources/TrailBlazer/Stat/StatDescriptor.swift
+++ Sources/TrailBlazer/Stat/StatDescriptor.swift
@@ -24,19 +24,6 @@ extension StatDescriptor {
     }
 
     /**
-    Updates the stat buffer with the latest information about the path
-
-    - Throws: `StatError.permissionDenied` when search permission is denied for one of the directories in the path prefix of path
-    - Throws: `StatError.badFileDescriptor` when the underlying file descript is not open or invalid
-    - Throws: `StatError.outOfMemory` when there is insufficient memory to fill the stat buffer
-    - Throws: `StatError.fileTooLarge` when the file descriptor refers to a file whose size, inode number, or number of blocks cannot be represented in, respectively, the types off_t, ino_t, or blkcnt_t
-    */
-    public mutating func update() throws {
-        let descriptor = try _descriptor ?! StatError.badFileDescriptor
-        try Self.update(descriptor, &_buffer)
-    }
-
-    /**
     Initializes a stat descriptor using the specified descriptor
 
     - Parameter descriptor: The opened descriptor to the path
--- Sources/TrailBlazer/Stat/StatPath.swift
+++ Sources/TrailBlazer/Stat/StatPath.swift
@@ -37,27 +37,6 @@ extension StatPath {
     }
 
     /**
-    Get information about a path
-
-    - Parameter options: The options to use for the stat API call
-
-    - Throws: `StatError.permissionDenied` when search permission is denied for one of the directories in the path prefix of pathname
-    - Throws: `StatError.badAddress` when the path points to a location outside you accessible address space
-    - Throws: `StatError.tooManySymlinks` when too many symlinks were encountered while resolving the path
-    - Throws: `StatError.pathnameTooLong` when the path has more than `PATH_MAX` number of characters
-    - Throws: `StatError.noRouteToPathname` when a component of the path does not exist or path is an empty string
-    - Throws: `StatError.outOfMemory` when there is insufficient memory to store the results of the stat API call
-    - Throws: `StatError.notADirectory` when a component of the path is not a directory
-    - Throws: `StatError.fileTooLarge` when the path refers to a file whose size, inode number, or number of blocks cannot be represented in, respectively, the types off_t, ino_t, or blkcnt_t
-    */
-    public mutating func update(options: StatOptions = []) throws {
-        var options = options
-        options.insert(self.options)
-        let path = try _path ?! RealPathError.emptyPath
-        try Self.update(path, options: options, &_buffer)
-    }
-
-    /**
     Initializes a stat path using a path
 
     - Parameter path: The path about which to retrieve information
