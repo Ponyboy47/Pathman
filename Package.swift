0dccb3c6268fd482d76c326714467ec7d45198b7
Jacob Williams <ponyboy47@gmail.com>
2019-07-01 19:16:16 -0600

    Get everything compiling and testing on macOS

--- Package.swift
+++ Package.swift
@@ -5,27 +5,28 @@ import PackageDescription
 
 let package = Package(
     name: "TrailBlazer",
+    platforms: [
+        .macOS(.v10_14)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "TrailBlazer",
-            targets: ["TrailBlazer"]),
+            targets: ["TrailBlazer"])
     ],
     dependencies: [
         .package(url: "https://github.com/Ponyboy47/ErrNo", from: "0.5.1"),
         .package(url: "https://github.com/Ponyboy47/Cdirent", from: "0.1.0"),
         .package(url: "https://github.com/Ponyboy47/Cglob", from: "0.1.0"),
-        .package(url: "https://github.com/Ponyboy47/SwiftShell", from: "4.2.0"),
+        .package(url: "https://github.com/kareman/SwiftShell", from: "5.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TrailBlazer",
             dependencies: ["ErrNo", "Cdirent", "Cglob"],
             exclude: ["Utilities/Autoclose"]),
         .testTarget(
             name: "TrailBlazerTests",
-            dependencies: ["TrailBlazer", "SwiftShell"]),
-    ]
+            dependencies: ["TrailBlazer", "SwiftShell"])
+    ],
+    swiftLanguageVersions: [.v5]
 )
